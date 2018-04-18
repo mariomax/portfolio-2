35e3c30199dab7bf1cde4fae324c675b9132d4f0
Mario Lopriore <mario@Marios-MBP.domain>
2018-04-18 12:29:50 -0700

    Added all 6 sections to content

--- includes/content.html.php
+++ includes/content.html.php
@@ -3,13 +3,6 @@
     <p>Here are a few of the sites I'm currently working on and a couple that are *live* in the wild. Nullam libero est, lacinia vel mauris in, condimentum tristique magna. Etiam vulputate, libero id accumsan sagittis, diam sem dignissim metus, et sagittis leo justo eu elit. Phasellus mollis viverra ex, sit amet convallis lorem auctor vel. Mauris sed urna arcu. Cras posuere augue non dictum pellentesque.</p>
 </section>
 <section class="thumbs">
-    <a href="https://www.worldwidewario.com/cms" target="_blank" rel="nofollow" class="overlay-text">
-        <img src="images/maynard.jpg" alt="the range finder">
-        <div class="overlay">
-            <h2 class="on-hover">Maynard's site</h2> 
-            <p class="p-on-hover">Praesent eget sollicitudin augue, id est lobortis lectus malesuada luctus. Nullam aliquet justo eget facilisis molestie.</p>   
-        </div>
-    </a>
     <a href="http://www.pccbridge.com" target="_blank" rel="nofollow" class="overlay-text">
         <img src="images/bridge.jpg" alt="PCC's student newspaper The Bridge">
         <div class="overlay">
@@ -24,24 +17,32 @@
             <p class="p-on-hover">Suspendisse ullamcorper convallis arcu at dapibus. Pellentesque consequat scelerisque nisi, id accumsan sem convallis vel.</p>   
         </div>
     </a>
-    <a href="#item4" class="overlay-text">
-        <img src="../images/greenSQ.jpg" alt="an image">
+    <a href="https://www.worldwidewario.com/cms" target="_blank" rel="nofollow" class="overlay-text">
+        <img src="images/maynard.jpg" alt="the range finder">
         <div class="overlay">
-            <h2 class="on-hover">See Page 5</h2>
-            <p>Maecenas porttitor lectus sit amet sem fermentum, vitae molestie ex ultricies. Quisque id elit vel ante accumsan scelerisque sit amet sit amet ante.  Nunc  vitae rutrum nisl. Mauris at volutpat sem. Nunc in magna velit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Ut vestibulum egestas lorem, et sollicitudin erat semper vel. Morbi quis sapien aliquam, euismod est fringilla, ornare dolor.</p>   
+            <h2 class="on-hover">Maynard's site</h2> 
+            <p class="p-on-hover">Praesent eget sollicitudin augue, id est lobortis lectus malesuada luctus. Nullam aliquet justo eget facilisis molestie.</p>   
         </div>
     </a>
-    <a href="#item5" class="overlay-text">
-        <img src="../images/maynardSQ.jpg" alt="an image">
+    <a href="https://www.anchortagdesign.com/ace" target="_blank" rel="nofollow" class="overlay-text">
+        <img src="images/ace-thumb.jpg" alt="Ace in the Hole MultiSports">
         <div class="overlay">
-            <h2 class="on-hover">See Page 6</h2>
-            <p>Integer ligula orci, blandit non posuere vel, ullamcorper ac leo. Ut euismod ex sit amet tempus condimentum.</p>  
+            <h2 class="on-hover">Ace in the Hole</h2>
+            <p class="p-on-hover">Maecenas porttitor lectus sit amet sem fermentum, vitae molestie ex ultricies. Quisque id elit vel ante accumsan scelerisque sit amet sit amet ante.  Nunc  vitae rutrum nisl. Mauris at volutpat sem. Nunc in magna velit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae. Morbi quis sapien aliquam, euismod est fringilla, ornare dolor.</p>   
         </div>
     </a>
-    <a href="#item6" class="overlay-text">
-        <img src="../images/burritoSQ.jpg" alt="an image">
+    <a href="https://www.anchortagdesign.com/coyote/index.html" target="_blank" rel="nofollow" class="overlay-text">
+        <img src="images/u-coyote.jpg" alt="Urban Coyote Project">
         <div class="overlay">
-            <h2 class="on-hover">See Page 7</h2>   
+            <h2 class="on-hover">Urban Coyotes</h2>
+            <p class="p-on-hover">Nunc  vitae rutrum nisl. Mauris at volutpat sem. Nunc in magna velit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae. Morbi quis sapien aliquam, euismod est fringilla, ornare dolor.</p>   
+        </div>
+    </a>
+    <a href="https://www.anchortagdesign.com/burrito/" target="_blank" rel="nofollow" class="overlay-text">
+        <img src="images/burrito.jpg" alt="Burrito Masala">
+        <div class="overlay">
+            <h2 class="on-hover">Burrito Masala</h2>
+            <p class="p-on-hover">Praesent eget sollicitudin augue, id est lobortis lectus malesuada luctus. Nullam aliquet justo eget facilisis molestie.</p>   
         </div>
     </a>
 </section>
